816d4b670187200befbc974567c33dfab3fdef1a
pabloleal7 <pabloleal7@hotmail.com>
2022-07-18 10:47:33 -0300

    a

--- src/main/java/com/example/keycloackjwt/KeycloackJwtApplication.java
+++ src/main/java/com/example/keycloackjwt/KeycloackJwtApplication.java
@@ -1,13 +1,32 @@
 package com.example.keycloackjwt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.example.keycloackjwt.config.KeycloakServerProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
+@EnableConfigurationProperties(KeycloakServerProperties.class)
 public class KeycloackJwtApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(KeycloackJwtApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(KeycloackJwtApplication.class, args);
+    }
+
+    @Bean
+    ApplicationListener<ApplicationReadyEvent> onApplicationReadyEventListener(ServerProperties serverProperties, KeycloakServerProperties keycloakServerProperties){
+        return (evt) -> {
+            Integer port = serverProperties.getPort();
+            String keycloakContextPath = keycloakServerProperties.getContextPath();
+            LOG.info("Embedded Keycloak started: http://localhost:{}{} to use keycloak", port, keycloakContextPath);
+        };
     }
 
 }
