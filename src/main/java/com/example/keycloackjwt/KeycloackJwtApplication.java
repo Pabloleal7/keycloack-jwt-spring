96c1acc6b0d691489c4e8c819968d31cbd58880e
pabloleal7 <pabloleal7@hotmail.com>
2022-07-18 09:39:34 -0300

    first commit

---
+++ src/main/java/com/example/keycloackjwt/KeycloackJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloackjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloackJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloackJwtApplication.class, args);
+    }
+
+}
